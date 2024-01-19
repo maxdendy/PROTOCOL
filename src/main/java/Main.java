91e85d91fe783af6dea6d649a07ffcf7230b2c15
maxdendy <95861147+maxdendy@users.noreply.github.com>
2024-01-19 07:49:53 +0600

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ import net.dv8tion.jda.api.utils.MemberCachePolicy;
 
 public class Main {
     public static void main(String[] args) {
-        JDA jda = JDABuilder.createDefault("OTE4NDIxMzYyMTQ4NzkwMzEy.GXWEJ-.D9dXeBf8997L4xJKkGxdyiwjv63JdCR7XeKEFA")
+        JDA jda = JDABuilder.createDefault("token")
                 //.enableIntents(GatewayIntent.GUILD_MEMBERS)
                 .setEnabledIntents(GatewayIntent.GUILD_MEMBERS, GatewayIntent.GUILD_VOICE_STATES, GatewayIntent.GUILD_EMOJIS_AND_STICKERS, GatewayIntent.SCHEDULED_EVENTS, GatewayIntent.GUILD_MESSAGES)
                 .setMemberCachePolicy(MemberCachePolicy.ALL)
